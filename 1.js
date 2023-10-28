fc00139b736a78986ca6375dba00a0b8f718699d
tilda-blocks-page-js <116749211+tilda-blocks-page@users.noreply.github.com>
2023-10-28 13:11:22 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,8 @@
+// Kodun 100 saniye sonra çalışmasını istediğiniz işlemi buraya ekleyin
+function kodunCalisacagiZaman() {
+    // Burada çalışmasını istediğiniz kodu ekleyin
+    console.log("Kod 10 saniye sonra çalıştı!");
+}
+
+// 100 saniye (100,000 milisaniye) sonra kodun çalışmasını başlat
+setTimeout(kodunCalisacagiZaman, 1000);
